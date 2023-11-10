d02a77c07379d32c676621987ffd36b3fe0b932d
xoxoonxu <145198399+xoxoonxu@users.noreply.github.com>
2023-11-10 18:48:58 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< Updated upstream
 import TabNavigation from "./navigations/Tab";
 import styled from "styled-components/native";
 import { NavigationContainer } from "@react-navigation/native";
@@ -9,30 +8,10 @@ const Container = styled.View`
   background-color: #ffffff;
   justify-content: center;
   align-items: center;
-=======
-import { theme } from "./theme";
-import styled, { ThemeProvider } from "styled-components/native";
-import { StatusBar } from "react-native";
-
-const Container = styled.View`
-  flex: 1;
-  backgroud-color: ${({ theme }) => theme.background};
-  align-items: center;
-  justify-content: flex-start;
-`;
-
-const Title = styled.Text`
-  color: ${({ theme }) => theme.black};
-  font-size: 28px;
-  font-weight: 700;
-  align-self: flex-start;
-  margin: 0px 20px;
->>>>>>> Stashed changes
 `;
 
 const App = () => {
   return (
-<<<<<<< Updated upstream
     <Container>
       <NavigationContainer>
         <TabNavigation />
@@ -41,17 +20,4 @@ const App = () => {
   );
 };
 
-=======
-    <ThemeProvider theme={theme}>
-      <Container>
-        <StatusBar
-          barStyle={"light-content"}
-          backgroundColor={theme.background}
-        ></StatusBar>
-        <Title>Title 페이지마다 다르게</Title>
-      </Container>
-    </ThemeProvider>
-  );
-};
->>>>>>> Stashed changes
 export default App;
