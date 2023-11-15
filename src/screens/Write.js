90dbe423036f23f0eb534b49cdb7166e0c1120b8
xoxoonxu <145198399+xoxoonxu@users.noreply.github.com>
2023-11-15 10:40:27 +0900

    색상 설정

--- src/screens/Write.js
+++ src/screens/Write.js
@@ -1,8 +1,24 @@
 import React from "react";
-import styled from "styled-components/native";
+import styled, { ThemeProvider } from "styled-components/native";
 import { Text } from "react-native";
+import { theme } from "../theme";
+
+//styled-component View
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+  align-items: center;
+  justify-content: flex-start;
+`;
+
 const Write = () => {
-  return <Text>write</Text>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Text>거래내역 입력</Text>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default Write;
