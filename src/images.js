be032428db4ee73750307dab956a931ef3742602
xoxoonxu <145198399+xoxoonxu@users.noreply.github.com>
2023-11-16 15:19:14 +0900

    다운

--- src/images.js
+++ src/images.js
@@ -1,13 +1,11 @@
 import homeIcon from "../assets/images/home_icon.png";
 import listIcon from "../assets/images/list_icon.png";
 import writeIcon from "../assets/images/write_icon.png";
-//import mainImage from '../assets/images/main_image.png';
-import pocket from '../assets/pocket.png';
+import pocket from "../assets/images/pocket.png";
 
 export const images = {
   home: homeIcon,
   list: listIcon,
   write: writeIcon,
-  // main: mainImage,
   pocket: pocket,
 };
