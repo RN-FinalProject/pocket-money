2d755fdda85bcdc3b41c3dbc9f302f996338c68d
xoxoonxu <145198399+xoxoonxu@users.noreply.github.com>
2023-11-18 16:26:58 +0900

    Update List.js

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled, { ThemeProvider } from "styled-components/native";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { Text } from "react-native";
 import { theme } from "../theme";
 import { Button } from "../components/Buttons";
 import { useNavigation } from "@react-navigation/native";
-import { Dimensions } from 'react-native';
-
+import { Dimensions } from "react-native";
 
 //styled-component View
 const Container = styled.View`
@@ -16,14 +15,13 @@ const Container = styled.View`
   justify-contents: center;
 `;
 
-
 const ItemContainer = styled.View`
   background-color: ${({ theme }) => theme.white};
   justify-contents: center;
   border-radius: 17px;
   width: 345px;
   height: 91px;
-  padding: 25px;
+  padding: 20px;
   margin: 5px 0px;
 `;
 
@@ -52,16 +50,15 @@ const Price = styled.Text`
   color: ${({ theme }) => theme.black};
 `;
 
-const Items = ({ date, content, price}) => {
+const Items = ({ date, content, price }) => {
   return (
-      <ItemContainer>
-        <Date>{date}</Date>
-        <TextContainer>
-          <Contents>{content}</Contents>
-          <Price>{price}</Price>
-        </TextContainer>
-      </ItemContainer>
-    
+    <ItemContainer>
+      <Date>{date}</Date>
+      <TextContainer>
+        <Contents>{content}</Contents>
+        <Price>{price}</Price>
+      </TextContainer>
+    </ItemContainer>
   );
 };
 
@@ -72,14 +69,13 @@ Items.propTypes = {
 };
 
 const Inventory = styled.ScrollView`
-  flex:1;
-  width: ${({ width}) => width - 60}px;
+  flex: 1;
+  width: ${({ width }) => width - 60}px;
   justify-contents: center;
   margin-top: 60px;
 `;
 
 const List = () => {
-
   const navigation = useNavigation();
 
   const handleButtonClick = () => {
@@ -87,7 +83,7 @@ const List = () => {
     navigation.navigate("거래내역 입력");
   };
 
-  const width = Dimensions.get('window').width;
+  const width = Dimensions.get("window").width;
 
   return (
     <ThemeProvider theme={theme}>
@@ -104,16 +100,14 @@ const List = () => {
         >
           거래내역 추가하기
         </Button>
-        <Inventory 
-          width={width}
-          showsVerticalScrollIndicator={false}>
-          <Items date="2023-11-17" content="넷플릭스" price="17000"/>
-          <Items date="2023-11-17" content="넷플릭스" price="17000"/>
-          <Items date="2023-11-17" content="넷플릭스" price="17000"/>
-          <Items date="2023-11-17" content="넷플릭스" price="17000"/>
-          <Items date="2023-11-17" content="넷플릭스" price="17000"/>
-          <Items date="2023-11-17" content="넷플릭스" price="17000"/>
-          <Items date="2023-11-17" content="넷플릭스" price="17000"/>
+        <Inventory width={width} showsVerticalScrollIndicator={false}>
+          <Items date="2023-11-17" content="넷플릭스" price="17000" />
+          <Items date="2023-11-17" content="넷플릭스" price="17000" />
+          <Items date="2023-11-17" content="넷플릭스" price="17000" />
+          <Items date="2023-11-17" content="넷플릭스" price="17000" />
+          <Items date="2023-11-17" content="넷플릭스" price="17000" />
+          <Items date="2023-11-17" content="넷플릭스" price="17000" />
+          <Items date="2023-11-17" content="넷플릭스" price="17000" />
         </Inventory>
       </Container>
     </ThemeProvider>
