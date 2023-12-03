daa695c0c5a2658d00989da38de6bd8b61fb33f6
YangEunji <129482143+heubanufi@users.noreply.github.com>
2023-12-03 14:49:35 +0900

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,12 +12,68 @@ const Container = styled.View`
   justify-content: flex-start;
 `;
 
+const TotalText = styled.Text`
+  color: #656565;
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+const Total = styled.Text`
+  font-size: 48px;
+  font-weight: 900;
+  color: ${({ theme }) => theme.positive};
+`;
+
+const PriceContainer = styled.View`
+  background-color: ${({ theme }) => theme.white};
+  justify-content: center;
+  border-radius: 17px;
+  padding: 20px;
+  margin: 40px 0px;
+  width: 383px;
+  height: 164px;
+  flex-direction: row;
+`;
+
+const PriceContainer2 = styled.View`
+  flex:1;
+`;
+
+const StrPrice = styled.Text`
+  flex: 1;
+  text-align: center;
+  font-size: 28px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.black};
+`;
+
+const NumPrice = styled.Text`
+  flex: 1;
+  text-align: center;
+  font-size: 32px;
+  font-weight: 900;
+  color: ${({ theme }) => theme.black};
+`;
+
+
+//total - 총금액, deposit - 입금, withdraw - 출금
 const Home = () => {
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Image source={images.pocket} />
-        <Text>거래내역</Text>
+        <TotalText>거래내역</TotalText>
+        <Total>300000원</Total>
+        <PriceContainer>
+          <PriceContainer2>
+            <StrPrice>입금</StrPrice>
+            <NumPrice>1000000000</NumPrice>
+          </PriceContainer2>
+          <PriceContainer2>
+            <StrPrice>출금</StrPrice>
+            <NumPrice>2000000000</NumPrice>
+          </PriceContainer2>
+        </PriceContainer>
       </Container>
     </ThemeProvider>
   );
